4ad889fd63e3802dbd7738f181bdf74d6d623882
Nanda Min Shin <nandaminshin2019@gmail.com>
2025-12-24 00:10:51 +0700

    fix auto logout bug

--- src/components/AuthNavigator.tsx
+++ src/components/AuthNavigator.tsx
@@ -12,26 +12,34 @@ const AuthNavigator = ({ children }: { children: React.ReactNode }) => {
 
     useEffect(() => {
         if (user) {
-            axios.get("/api/fmiis-backend/v001/auth/me");
-            if (user.role === 'dev' && !location.pathname.startsWith('/dev')) {
-                navigate('/dev');
-            } else if (user.role === 'glob' && !location.pathname.startsWith('/glob')) {
-                navigate('/glob');
+            const run = async () => {
+                try {
+                    await axios.get("/api/fmiis-backend/v001/auth/me");
+                } catch {
+                    return;
+                }
 
-            } else if (user.role === 'gm' && !location.pathname.startsWith('/gm-md')) {
-                navigate('/gm-md');
+                if (user.role === 'dev' && !location.pathname.startsWith('/dev')) {
+                    navigate('/dev');
+                } else if (user.role === 'glob' && !location.pathname.startsWith('/glob')) {
+                    navigate('/glob');
 
-            } else if (user.role === 'md' && !location.pathname.startsWith('/gm-md')) {
-                navigate('/gm-md');
+                } else if (user.role === 'gm' && !location.pathname.startsWith('/gm-md')) {
+                    navigate('/gm-md');
 
-            } else if (user.role === 'hr' && !location.pathname.startsWith('/hr')) {
-                navigate('/hr');
+                } else if (user.role === 'md' && !location.pathname.startsWith('/gm-md')) {
+                    navigate('/gm-md');
 
-            } else if ((user.role !== 'gm' && user.role !== 'md') && location.pathname.startsWith('/gm-md')) {
-                navigate('/');
-            } else if (user.role !== 'hr' && location.pathname.startsWith('/hr')) {
-                navigate('/');
+                } else if (user.role === 'hr' && !location.pathname.startsWith('/hr')) {
+                    navigate('/hr');
+
+                } else if ((user.role !== 'gm' && user.role !== 'md') && location.pathname.startsWith('/gm-md')) {
+                    navigate('/');
+                } else if (user.role !== 'hr' && location.pathname.startsWith('/hr')) {
+                    navigate('/');
+                }
             }
+            run();
         }
     }, [user, navigate, location.pathname]);
     return children
